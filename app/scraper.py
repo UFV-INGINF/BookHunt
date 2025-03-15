ba7865adf1196e243bbf8a395c4607aaa871cde9
Carmen <carmen.moreno.udaondo@gmail.com>
2025-03-15 21:28:50 +0100

    Arreglado scrapping casa del libro

--- app/scraper.py
+++ app/scraper.py
@@ -1,4 +1,6 @@
 from playwright.sync_api import sync_playwright  # type: ignore
+import re
+import requests
 
 
 class Libro:
@@ -23,73 +25,94 @@ class Libro:
         return self.precio + self.gastos_envio
 
 
-def scrape_casa_del_libro(isbn_libro):
-    url = f"https://www.casadellibro.com/?query={isbn_libro}"
-
+def obtener_link_libro_playwright_casalibro(isbn):
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
+        browser = p.chromium.launch(
+            headless=True
+        )  # Cambia a True si no necesitas ver el navegador
         page = browser.new_page()
 
-        headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36"
-        }
-        page.set_extra_http_headers(headers)
+        # Simulación de usuario real
+        page.set_extra_http_headers(
+            {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
+            }
+        )
 
-        page.goto(url)
+        # URL de búsqueda en Casa del Libro
+        search_url = f"https://www.casadellibro.com/?query={isbn}"
+        page.goto(search_url, timeout=60000)
 
-        # Esperar hasta que el contenido de la página esté completamente cargado
-        page.wait_for_load_state("domcontentloaded", timeout=30000)
-
-        libros = []
-
+        # Esperar a que el enlace del libro aparezca (máximo 10 segundos)
         try:
-            page.wait_for_selector(".x-result__description", timeout=10000)
+            page.wait_for_selector("a.x-result-link", timeout=10000)
+            link_element = page.query_selector("a.x-result-link")
+        except:
+            link_element = None
 
-            items = page.query_selector_all(".x-result__description")
-
-            for item in items:
-                try:
-                    nombre_element = item.query_selector('h2[data-test="result-title"]')
-                    if nombre_element:
-                        nombre = nombre_element.inner_text().strip()
-                    else:
-                        nombre = "Nombre no disponible"
-
-                    precio_element = item.query_selector(".x-currency")
-                    if precio_element:
-                        precio = (
-                            precio_element.inner_text()
-                            .strip()
-                            .replace("€", "")
-                            .replace(",", ".")
-                        )
-                    else:
-                        precio = "0.0"
-
-                    try:
-                        precio = float(precio)
-                    except ValueError:
-                        precio = 0.0
-
-                    # El enlace será el de la página de búsqueda
-                    enlace = f"https://www.casadellibro.com/?query={isbn_libro}"
-
-                    libro = Libro(
-                        nombre=nombre,
-                        isbn=isbn_libro,
-                        tienda="La Casa del Libro",
-                        precio=precio,
-                        gastos_envio=3.0,
-                        enlace=enlace,  # Añadir el enlace de búsqueda
-                    )
-                    libros.append(libro)
-                except Exception as e:
-                    print(f"Error al extraer información de un producto: {e}")
-
-        except Exception as e:
-            print(f"Error general: {e}")
+        if link_element:
+            book_href = link_element.get_attribute("href")
+            book_url = (
+                book_href
+                if "https" in book_href
+                else f"https://www.casadellibro.com{book_href}"
+            )
+        else:
+            book_url = "No encontrado"
 
         browser.close()
+
+    return book_url
+
+
+def scrape_casa_del_libro(isbn_libro):
+    libros = []
+    url_libro = obtener_link_libro_playwright_casalibro(isbn_libro)
+
+    response = requests.get(
+        url_libro,
+        headers={
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
+        },
+    )
+
+    # Extraer la información de la etiqueta title
+    title_match = re.search(r"<title>(.*?)</title>", response.text)
+    book_title = "Nombre no disponible"
+    price = 0.0
+
+    if title_match:
+        title_content = title_match.group(1)
+        # El formato esperado es: "TÍTULO | AUTOR | EDITORIAL | Casa del Libro"
+        parts = title_content.split(" | ")
+
+        if len(parts) >= 3:
+            book_title = parts[0].strip()
+            author = parts[1].strip()
+            editorial = parts[2].strip() if len(parts) > 2 else "N/A"
+
+            # Extracción de precio usando regex (buscando formato xx.xx EUR)
+            price_match = re.search(r'"(\d+\.\d+)\s+EUR"', response.text)
+            if price_match:
+                # Convertir el precio a float y aplicar descuento
+                price = float(price_match.group(1)) - (
+                    float(price_match.group(1)) * 0.05
+                )
+                # Redondear a 2 decimales
+                price = round(price, 2)
+
+    # El enlace será el de la página del libro (no de búsqueda)
+    enlace = url_libro
+    libro = Libro(
+        nombre=book_title.capitalize(),
+        isbn=isbn_libro,
+        tienda="La Casa del Libro",
+        precio=price,
+        gastos_envio=0.0,
+        enlace=enlace,  # Añadir el enlace de búsqueda
+    )
+    libros.append(libro)
+
     return libros
 
 
@@ -146,6 +169,7 @@ def scrape_iberlibro(isbn_libro):
 
     return libros
 
+
 def scrape_agapea(isbn_libro):
     url = f"https://www.agapea.com/buscar/buscador.php?texto={isbn_libro}"
 
@@ -159,18 +183,26 @@ def scrape_agapea(isbn_libro):
         page.set_extra_http_headers(headers)
         page.goto(url)
         page.wait_for_load_state("domcontentloaded", timeout=10000)
-        
+
         libros = []
 
         try:
-            
-            nombre_element = page.query_selector('h1')
-            nombre = nombre_element.inner_text().strip() if nombre_element else "Nombre no disponible"
-            precio_element = page.query_selector('.precio strong')
-            precio_texto = precio_element.inner_text().strip() if precio_element else "0"
+
+            nombre_element = page.query_selector("h1")
+            nombre = (
+                nombre_element.inner_text().strip()
+                if nombre_element
+                else "Nombre no disponible"
+            )
+            precio_element = page.query_selector(".precio strong")
+            precio_texto = (
+                precio_element.inner_text().strip() if precio_element else "0"
+            )
             precio = float(precio_texto.replace("€", "").replace(",", ".").strip())
             envio_element = page.query_selector(".envio-gratis")
-            gastos_envio = 0 if envio_element else 3.99  # Suponiendo que el envío cuesta 3.99 si no es gratis
+            gastos_envio = (
+                0 if envio_element else 3.99
+            )  # Suponiendo que el envío cuesta 3.99 si no es gratis
 
             libro = Libro(
                 nombre=nombre,
@@ -180,7 +212,7 @@ def scrape_agapea(isbn_libro):
                 gastos_envio=gastos_envio,
                 enlace=url,
             )
-                    
+
             libros.append(libro)
 
         except Exception as e:
@@ -188,6 +220,7 @@ def scrape_agapea(isbn_libro):
         browser.close()
 
     return libros
+
 
 def scrapear_libros(isbn_libro):
     libros = []
